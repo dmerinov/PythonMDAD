d8bac0f15f0e2282677972360f988a5467c9bad5
David Merino <merinovecinod@gmail.com>
2023-05-03 20:43:17 +0200

    CU1- Added one plot with 3 vars (3 dims)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
+from matplotlib import ticker
+
 from connection.Firebase import *
 from model.Product import ProductItem
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def init():
@@ -17,11 +21,42 @@ def init():
         product_model_list.append(
             ProductItem(item.get("nombre"), item.get("stock"), item.get("tienda"), item.get("precio")))
 
-    stock_location_graph(product_model_list)
-    price_location_graph(product_model_list)
+    # stock_location_graph(product_model_list)
+    # price_location_graph(product_model_list)
+    stock_price_location_graph(product_model_list)
 
 
 # ejemplo_iris():
+
+def stock_price_location_graph(product_model_list):
+    price = []
+    shop = []
+    stock = []
+
+    for item in product_model_list:
+        print(f'name: {item.name}')
+        print(f'stock: {item.stock}')
+        print(f'shop: {item.shop}')
+        print(f'precio: {item.price}')
+        print("---------------")
+        shop.append(item.shop)
+        price.append(item.price)
+        stock.append(item.stock)
+
+    compound_graph = pd.DataFrame(
+        {
+            'shop': shop,
+            'stock': stock,
+            'price': price
+        },
+        index=shop
+    )
+
+    plt.plot(shop, price, "-b", label="price", color='purple', linewidth=3)
+    plt.bar(shop, stock, alpha=0.5, color='orange', label="stock")
+    plt.legend(loc="upper left")
+    plt.show()
+
 
 def price_location_graph(product_model_list):
     x = []
